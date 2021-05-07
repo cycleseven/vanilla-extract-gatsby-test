92a6154b61f250fd6c6cf73ea4e0ed36cf994d67
cycleseven <unvexer@gmail.com>
2021-05-07 16:33:03 +0100

    Force latest versions of @vanilla-extract dependencies to fix build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,13 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Good from "../components/good";
-
-// Uncomment this import to break the build
-// import Bad from "../components/bad";
+import Bad from "../components/bad";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Good />
+    <Bad />
   </Layout>
 )
 
